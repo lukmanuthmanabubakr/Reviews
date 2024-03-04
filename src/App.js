14a40b00005bb02aa46fe35124f21a70daa7c7d0
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-03-04 09:41:58 +0100

    legend

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <main>
       <section className='container'>
         <div className='title'>
-          <h2>our reviews</h2>
+          <h2>Legend reviews</h2>
           <div className='underline'></div>
         </div>
         <Review />
